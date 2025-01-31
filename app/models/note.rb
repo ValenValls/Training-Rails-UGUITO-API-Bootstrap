6efc1ff0962ed895c4fc003586500c23966944af
ValenValls <valentinvalls@hotmail.com>
2025-01-31 09:30:43 -0300

    feat(note): add utility to note

--- app/models/note.rb
+++ app/models/note.rb
@@ -9,12 +9,13 @@
 #  user_id    :bigint(8)        not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  utility_id :bigint(8)        not null
 #
 class Note < ApplicationRecord
-  validates :user_id, :content, :note_type, :title,
+  validates :user_id, :utility_id, :content, :note_type, :title,
             presence: true
 
   belongs_to :user
-
+  belongs_to :utility
   enum note_type: { review: 0, critique: 1 }
 end
