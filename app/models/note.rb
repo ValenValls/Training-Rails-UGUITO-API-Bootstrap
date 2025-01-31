962453c7055d40626e6a0c33a0b350c0be010bec
ValenValls <valentinvalls@hotmail.com>
2025-01-31 15:04:09 -0300

    feat(note): add content_length and review validation

--- app/models/note.rb
+++ app/models/note.rb
@@ -14,6 +14,7 @@
 class Note < ApplicationRecord
   validates :user_id, :utility_id, :content, :note_type, :title,
             presence: true
+  validate :validate_word_count
 
   belongs_to :user
   belongs_to :utility
@@ -21,5 +22,27 @@ class Note < ApplicationRecord
 
   def word_count
     content.split.size
+  end
+
+  def content_length
+    short_word_count_threshold = utility.short_word_count_threshold
+    medium_word_count_threshold = utility.medium_word_count_threshold
+    return 'short' if word_count <= short_word_count_threshold
+    return 'medium' if word_count <= medium_word_count_threshold
+    'long'
+  end
+
+  def review_exceeds_word_limit?
+    note_type == 'review' && content_length != 'short'
+  end
+
+  def validate_word_count
+    return unless utility && review_exceeds_word_limit?
+    error_message = I18n.t(
+      'error_node_validation_word_limit',
+      utility_name: utility.name,
+      limit: utility.short_word_count_threshold
+    )
+    errors.add(:content, error_message)
   end
 end
